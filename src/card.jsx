e6930bc94b077b9d6b5fda61c4d609d5661790ea
Cameron Bourke <cameron2295@gmail.com>
2016-05-02 23:21:12 +1000

    Fix linting errors for both Card and Cardstack components

--- src/card.jsx
+++ src/card.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 
 class Card extends React.Component {
-	constructor(props) {
-	  super(props);
+	constructor (props) {
+		super(props);
 		this.state = {
-			hover: false
+			hover: false,
 		};
 	}
 
-	handleClick() {
+	handleClick () {
 		const {cardId, cardClicked} = this.props;
-		this.props.handleClick(cardId, cardClicked);
+		this.props.onClick(cardId, cardClicked);
 		this.setState({ hover: false });
 	}
 
-	render() {
+	render () {
 		const {cardId, cardSelected, topOffset, hoverOffset} = this.props;
-		const offset = (cardId !== 0) && this.state.hover && !cardSelected ? hoverOffset: 0;
+		const offset = (cardId !== 0) && this.state.hover && !cardSelected ? hoverOffset : 0;
+		const transform = `translate3d(0,${topOffset - offset}px,0)`;
 		const cardStyles = {
 			...styles,
-		  background: this.props.background,
-		  transform: `translate3d(0,${this.props.topOffset - offset}px,0)`,
-		  height: this.props.height
-	  };
+			background: this.props.background,
+			transform,
+			WebkitTransform: transform,
+			height: this.props.height,
+		};
 		return (
 			<li
 				style={cardStyles}
@@ -37,10 +39,11 @@ class Card extends React.Component {
 
 const styles = {
 	position: 'absolute',
-  top: 0,
+	top: 0,
 	width: '100%',
 	cursor: 'pointer',
-	transition: '0.5s transform ease'
+	transition: '0.5s transform ease',
+	WebkitTransition: '-webkit-transform 0.5s ease',
 };
 
 export default Card;
--- src/cardstack.jsx
+++ src/cardstack.jsx
@@ -1,92 +1,73 @@
 import React from 'react';
 
-const styles = {
-	card: {
-		position: 'absolute',
-	  top: 0,
-		width: '100%',
-		cursor: 'pointer',
-		transition: '0.5s transform ease',
-		WebkitTransition: '-webkit-transform 0.5s ease'
-	}
-};
-
-const throwError = (condition, message) => {
-	if(condition) {
-		throw new Error(message);
-	}
-};
-
 const equalsZero = (num) => num === 0;
+const errorMessage = 'CardStack component must have at least two child Card components. Please check the children of this CardStack instance.';
 
 class CardStack extends React.Component {
-	constructor(props) {
+	constructor (props) {
 		super(props);
 		const childrenLength = props.children.length || 1;
 		const headerHeight = props.height / childrenLength;
 
-		throwError(childrenLength <= 1, 'CardStack component must have at least two child Card components. Please check the children of this CardStack instance.');
+		if (childrenLength <= 1) throw new Error(errorMessage);
 
-		this.initialTopOffsets = props.children.map((child, i) => equalsZero(i) ? 0: headerHeight * i);
+		this.initialTopOffsets = props.children.map((child, i) => equalsZero(i) ? 0 : headerHeight * i);
 
 		this.state = {
 			topOffsets: this.initialTopOffsets,
-			cardSelected: false
+			cardSelected: false,
 		};
 	}
 
-	handleCardClick(id, cb) {
-		let state = {
+	handleCardClick (id, cb) {
+		const initialState = {
 			topOffsets: [],
-			cardSelected: true
+			cardSelected: true,
+		};
+		const {cardSelected} = this.state;
+
+		const nextState = (prev, offset, index) => {
+			const newOffset = (index === id) ? 0 : this.props.height;
+			return {
+				cardSelected: cardSelected ? false : true,
+				topOffsets: [
+					...prev.topOffsets,
+					cardSelected ? this.initialTopOffsets[index] : newOffset,
+				],
+			};
 		};
 
-		this.setState(
-			this.state.topOffsets.reduce((prev, offset, index) => {
-				let newOffset = (index === id) ? 0 : this.props.height;
+		this.setState(this.state.topOffsets.reduce(nextState, initialState));
 
-				if(this.state.cardSelected) {
-					prev.cardSelected = false;
-					newOffset = this.initialTopOffsets[index];
-				}
-
-				prev.topOffsets.push(newOffset);
-
-				return prev;
-			}, state)
-		);
-
-		if(cb) {
-			cb(this.state.cardSelected);
-		};
+		if (cb) cb(this.state.cardSelected, id);
 	}
 
-	renderCards() {
-		return this.props.children.map((child, i) => (
-			React.cloneElement(child, {
-				key: i,
-				cardId: i,
-				hoverOffset: this.props.hoverOffset,
-				cardSelected: this.state.cardSelected,
-				height: this.props.height,
-				topOffset: this.state.topOffsets[i],
-				handleClick: this.handleCardClick.bind(this)
-			})
-		));
+	renderCards () {
+		const cloneCard = (child, i) => React.cloneElement(child, {
+			key: i,
+			cardId: i,
+			hoverOffset: this.props.hoverOffset,
+			cardSelected: this.state.cardSelected,
+			height: this.props.height,
+			topOffset: this.state.topOffsets[i],
+			onClick: this.handleCardClick.bind(this),
+		});
+
+		return this.props.children.map(cloneCard);
 	}
 
-	render() {
+	render () {
 		const stackStyles = {
 			...styles,
 			background: this.props.background,
 			height: this.props.height,
 			width: this.props.width,
-	  };
+		};
 		return (
-      <ul style={stackStyles}>
-			  {this.renderCards()}
-		  </ul>
-	  );
+			<ul style={stackStyles}>
+				{this.renderCards()}
+			</ul>
+		);
 	}
 }
 
@@ -96,43 +77,21 @@ const styles = {
 	position: 'relative',
 	overflow: 'hidden',
 	padding: 0,
-	margin: 0
+	margin: 0,
 };
 
-<<<<<<< HEAD
 CardStack.propTypes = {
-	width: React.PropTypes.number,
-	height: React.PropTypes.number,
 	background: React.PropTypes.string,
-	hoverOffset: React.PropTypes.number
+	height: React.PropTypes.number,
+	hoverOffset: React.PropTypes.number,
+	width: React.PropTypes.number,
 };
-=======
-	render() {
-		const hoverOffset = this.props.cardId !== 0 && this.state.hover && !this.props.cardSelected ? this.props.hoverOffset: 0;
-		const dynamicStyles = {
-		  background: this.props.background,
-		  transform: `translate3d(0,${this.props.topOffset - hoverOffset}px,0)`,
-          WebkitTransform: 'translate3d(0,' + (this.props.topOffset - hoverOffset) + 'px,0)',
-		  height: this.props.height
-	  };
-		return (
-			<li
-				style={Object.assign({}, styles.card, dynamicStyles)}
-				onClick={this.handleClick.bind(this)}
-				onMouseEnter={this.setHoverState.bind(this, true)}
-				onMouseLeave={this.setHoverState.bind(this, false)}>
-					{this.props.children}
-			</li>
-		);
-	}
-}
->>>>>>> 603a4e3f7bcb24474b36b74c284fe82fd37d7a56
 
 CardStack.defaultProps = {
 	width: 350,
 	height: 600,
 	bgColor: 'f8f8f8',
-	hoverOffset: 30
+	hoverOffset: 30,
 };
 
 export default CardStack;
